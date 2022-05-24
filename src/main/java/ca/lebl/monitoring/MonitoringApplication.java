a84ff7cc1a70c47b53562898ac653c3cb185412b
Matt Lebl <lebl.matt@gmail.com>
2022-05-24 14:43:55 +0200

    Change dll to "create-drop"
    So that testing during initial development can be faster

--- src/main/java/ca/lebl/monitoring/MonitoringApplication.java
+++ src/main/java/ca/lebl/monitoring/MonitoringApplication.java
@@ -1,13 +1,47 @@
 package ca.lebl.monitoring;
 
+import ca.lebl.monitoring.entity.User;
+import ca.lebl.monitoring.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class MonitoringApplication {
+public class MonitoringApplication implements CommandLineRunner {
+
+	private UserRepository userRepository;
+
+	public MonitoringApplication(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonitoringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Seed database with some users
+		User applifting = new User(
+			"Applifting",
+			"info@applifting.cz",
+			"93f39e2f-80de-4033-99ee-249d92736a25"
+		);
+
+		User batman = new User(
+			"Batman",
+			"batman@example.com",
+			"dcb20f8a-5657-4f1b-9f7f-ce65739b359e"
+		);
+
+		User matt = new User(
+			"Matt",
+			"matt.lebl@applifting.cz",
+			"hello"
+		);
+
+		userRepository.saveAll(List.of(applifting, batman, matt));
+	}
 }
