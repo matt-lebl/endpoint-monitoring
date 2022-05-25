393933590273134ba18dbe859edff349576bd2b5
Matt Lebl <lebl.matt@gmail.com>
2022-05-25 10:06:13 +0200

    Switch to UserService for database seeding

--- src/main/java/ca/lebl/monitoring/MonitoringApplication.java
+++ src/main/java/ca/lebl/monitoring/MonitoringApplication.java
@@ -1,7 +1,7 @@
 package ca.lebl.monitoring;
 
 import ca.lebl.monitoring.entity.User;
-import ca.lebl.monitoring.repository.UserRepository;
+import ca.lebl.monitoring.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +11,10 @@ import java.util.List;
 @SpringBootApplication
 public class MonitoringApplication implements CommandLineRunner {
 
-	private UserRepository userRepository;
+	private UserService userService;
 
-	public MonitoringApplication(UserRepository userRepository) {
-		this.userRepository = userRepository;
+	public MonitoringApplication(UserService userService) {
+		this.userService = userService;
 	}
 
 	public static void main(String[] args) {
@@ -24,24 +24,22 @@ public class MonitoringApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// Seed database with some users
-		User applifting = new User(
+		userService.createUser(
 			"Applifting",
 			"info@applifting.cz",
 			"93f39e2f-80de-4033-99ee-249d92736a25"
 		);
 
-		User batman = new User(
+		userService.createUser(
 			"Batman",
 			"batman@example.com",
 			"dcb20f8a-5657-4f1b-9f7f-ce65739b359e"
 		);
 
-		User matt = new User(
+		userService.createUser(
 			"Matt",
 			"matt.lebl@applifting.cz",
 			"hello"
 		);
-
-		userRepository.saveAll(List.of(applifting, batman, matt));
 	}
 }
