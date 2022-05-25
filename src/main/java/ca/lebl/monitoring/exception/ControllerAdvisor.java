d52de75fa57860fd44c5e9d47114082b7f814f3a
Matt Lebl <lebl.matt@gmail.com>
2022-05-25 17:09:27 +0200

    Fix validation error message

--- src/main/java/ca/lebl/monitoring/exception/ControllerAdvisor.java
+++ src/main/java/ca/lebl/monitoring/exception/ControllerAdvisor.java
@@ -1,13 +1,15 @@
 package ca.lebl.monitoring.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,13 +24,19 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
         return notFoundResponse();
     }
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Object> handleConstraintViolation(
-        ConstraintViolationException e
-    ) {
-        // thank you https://medium.com/@aamine/customized-input-validation-in-spring-boot-1927aa440bc6
-        String exceptionResponse = String.format("Invalid input parameters: %s\n", e.getMessage());
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        StringBuilder exceptionResponse = new StringBuilder();
+
+        exceptionResponse.append("Invalid input parameters: ");
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            exceptionResponse.append("[");
+            exceptionResponse.append(error.getField() + ": " + error.getDefaultMessage());
+            exceptionResponse.append("] ");
+        }
+
+        return new ResponseEntity<>(exceptionResponse.toString(), HttpStatus.BAD_REQUEST);
     }
 
     private ResponseEntity<Object> notFoundResponse() {
