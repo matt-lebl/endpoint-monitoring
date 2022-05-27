33868ad25604bc978effadd85fc6bca0d9debe6f
Matt Lebl <lebl.matt@gmail.com>
2022-05-27 13:29:21 +0200

    Fix endpoint rescheduling

--- src/main/java/ca/lebl/monitoring/service/MonitoredEndpointService.java
+++ src/main/java/ca/lebl/monitoring/service/MonitoredEndpointService.java
@@ -72,6 +72,8 @@ public class MonitoredEndpointService {
                 response.getStatusCodeValue(),
                 response.getBody()
             );
+
+            rescheduleEndpointCheck(endpoint);
         }
     }
 
